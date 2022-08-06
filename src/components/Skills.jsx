da1846cbf9dc10243fdf938e4642cfa0af717dde
Andrew_Gomez <keepitgomez@yahoo.com>
2022-08-06 14:17:11 -0700

    icon cards rounded

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -16,8 +16,8 @@ const Skills = () => {
         {/** Container */}
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify center w-full h-full'>
             <div>
-                <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Experience</p>
-                <p className='py-4'>// These are the technologies I've recently worked with</p>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Skills</p>
+                <p className='py-4'>These are the technologies I've recently worked with</p>
             </div>
 
             <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
@@ -25,31 +25,31 @@ const Skills = () => {
                     <img className='w-20 mx-auto' src={JavaScript} alt="JavaScript icon" />
                     <p className='my-4'>JavaScript</p>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded'>
                     <img className='w-20 mx-auto' src={ReactImg} alt="React icon" />
                     <p className='my-4'>React</p>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded'>
                     <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
                     <p className='my-4'>HTML</p>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded'>
                     <img className='w-20 mx-auto' src={CSS} alt="CSS icon" />
                     <p className='my-4'>CSS</p>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded'>
                     <img className='w-20 mx-auto' src={Tailwind} alt="Tailwind icon" />
                     <p className='my-4'>Tailwind</p>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded'>
                     <img className='w-20 mx-auto' src={Node} alt="Github icon" />
                     <p className='my-4'>Node Js</p>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded'>
                     <img className='w-20 mx-auto' src={FireBase} alt="Mongo icon" />
                     <p className='my-4'>FireBase</p>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded'>
                     <img className='w-20 mx-auto' src={Mongo} alt="FireBase icon" />
                     <p className='my-4'>Mongo DB</p>
                 </div>
