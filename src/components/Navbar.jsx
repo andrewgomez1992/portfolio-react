bf195f782f1586f9721d0c2de5fd6807f9f67599
Andrew_Gomez <keepitgomez@yahoo.com>
2022-08-05 20:27:08 -0700

    *bg black

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import {FaBars, FaTimes} from 'react-icons';
-import logo from '../assets/initials.png';
+import {FaBars, FaTimes} from 'react-icons/fa';
+import Logo from '../assets/initials.png';
 
 const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-
+        <div>
+            <img src={Logo} alt="Logo Image" />
+        </div>
     </div>
   )
 }
