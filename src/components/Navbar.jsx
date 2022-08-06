fe9f9bdf4aae4a7147738df043061298e39119eb
Andrew_Gomez <keepitgomez@yahoo.com>
2022-08-06 08:39:40 -0700

    logo fully responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,12 @@ import Logo from '../assets/Logo.png';
 const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-        <div className='relative right-20'>
+        <div className='relative right-20 md:right-20'>
             <img src={Logo} alt="Logo Image" style ={{width: '200px'}}/>
         </div>
 
         {/** Menu */}
-        <div>
+        <div className='hidden md:flex'>
             <ul className='flex'>
                 <li>Home</li>
                 <li>About</li>
@@ -37,7 +37,7 @@ const Navbar = () => {
 
 
         {/** Social Icons */}
-        <div></div>
+        <div className='hidden'></div>
     </div>
   )
 }
