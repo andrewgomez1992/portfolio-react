d54bcef68865f8c6996fe07e72bdeb50019e7e42
Andrew_Gomez <keepitgomez@yahoo.com>
2022-08-06 13:41:25 -0700

    skills component added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,8 +12,8 @@ const About = () => {
 
                 </div>
                 </div>
-                <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
-                    <div>
+                <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+                    <div className='sm:text-right text-4xl font-bold '>
                         <p>Hi. I'm Andrew, nice to meet you. Please take a look around.</p>
                     </div>
                     <div>
