391172660844cb1a072b3a478e40fc3c8f877403
Andrew_Gomez <keepitgomez@yahoo.com>
2022-08-06 14:22:35 -0700

    work.jsx frame wip

---
+++ src/components/Work.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+        <div>
+            <div>
+                <p>Projects</p>
+                <p>Check out some of my recent projects</p>
+            </div>
+
+            <div>
+                <div>
+                    <div>
+                        <span>
+
+                        </span>
+                        <div>
+                            <a href="">
+                                <button></button>
+                            </a>
+                            <a href="">
+                                <button></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Work
