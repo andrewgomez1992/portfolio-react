0be83ad1246e4ff7eeae77992b0225fcc745b0d6
Andrew_Gomez <keepitgomez@yahoo.com>
2022-08-06 13:02:10 -0700

    home finished, not about

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
             <p className='text-pink-600'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Andrew Gomez</h1>
-            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer</h2>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer.</h2>
             <p className='text-[#8892b0] py-4 max-w-[700px]'>I would consider myself a very thoughtful and creative individual. 
         Supported with emotional intelligence. I have a very clear understanding 
         of the needs of those around me. I am very curious, if I had 100 lifetimes 
