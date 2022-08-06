d8772bf5df5460f1ef846141b48c06d0b91ca337
Andrew_Gomez <keepitgomez@yahoo.com>
2022-08-06 12:17:15 -0700

    social icon colors set

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,19 +51,19 @@ const Navbar = () => {
                         Linkedin <FaLinkedin  size={30}/>
                     </a>
                 </li>
-                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 rounded'>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333] rounded'>
                     <a className='flex justify-between items center w-full text-gray-300' href="/">
-                        Linkedin <FaGithub  size={30}/>
+                        Github <FaGithub  size={30}/>
                     </a>
                 </li>
-                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 rounded'>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0] rounded'>
                     <a className='flex justify-between items center w-full text-gray-300' href="/">
-                        Linkedin <HiOutlineMail  size={30}/>
+                        Email <HiOutlineMail  size={30}/>
                     </a>
                 </li>
-                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 rounded'>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69] rounded'>
                     <a className='flex justify-between items center w-full text-gray-300' href="/">
-                        Linkedin <BsFillPersonLinesFill  size={30}/>
+                        Resume <BsFillPersonLinesFill  size={30}/>
                     </a>
                 </li>
             </ul>
