6b4d85202ed77a43a3ccf189f8eb399d36604bc9
Andrew_Gomez <keepitgomez@yahoo.com>
2022-08-06 13:54:04 -0700

    text color and padding changed on skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,16 +12,16 @@ import Mongo from '../assets/mongo.png'
 
 const Skills = () => {
   return (
-    <div name='skills'>
+    <div name='skills' className='bg-[#0a192f] text-gray-300'>
         {/** Container */}
-        <div>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify center w-full h-full'>
             <div>
-                <p>Experience</p>
-                <p>// These are the technologies I've recently worked with</p>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Experience</p>
+                <p className='py-4'>// These are the technologies I've recently worked with</p>
             </div>
             <div>
                 <div>
-                    <img src={HTML} alt="HTML icon" />
+                    <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
                     <p>HTML</p>
                 </div>
             </div>
