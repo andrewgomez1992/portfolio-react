32fb9faed92c59d9006190ccd4f242cd40cebebb
Andrew_Gomez <keepitgomez@yahoo.com>
2022-08-05 20:35:27 -0700

    logo positioned on nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {FaBars, FaTimes} from 'react-icons/fa';
-import Logo from '../assets/initials.png';
+import Logo from '../assets/Logo.png';
 
 const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-        <div>
-            <img src={Logo} alt="Logo Image" />
+        <div className='relative right-20'>
+            <img src={Logo} alt="Logo Image" style ={{width: '200px'}}/>
         </div>
     </div>
   )
