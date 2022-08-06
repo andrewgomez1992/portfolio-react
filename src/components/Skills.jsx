932725a66502cd8af057516f34e2f343e4ef9645
Andrew_Gomez <keepitgomez@yahoo.com>
2022-08-06 13:48:54 -0700

    skills component added

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import JavaScript from '../assets/javascript.png'
+import ReactImg from '../assets/react.png'
+import Node from '../assets/node.png'
+import FireBase from '../assets/firebase.png'
+import Github from '../assets/github.png'
+import Tailwind from '../assets/tailwind.png'
+import Mongo from '../assets/mongo.png'
+
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div name='skills'>
+        {/** Container */}
+        <div>
+            <div>
+                <p>Experience</p>
+                <p>// These are the technologies I've recently worked with</p>
+            </div>
+            <div>
+                <div>
+                    <img src={HTML} alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
