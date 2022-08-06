a727955a92db36a881353495c4916972723a5fe9
Andrew_Gomez <keepitgomez@yahoo.com>
2022-08-06 16:00:10 -0700

    updated theme

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 
         {/** Container */}
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <p className='text-pink-600'>Hi, my name is</p>
+            <p className='text-[#41c69e]'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Andrew Gomez</h1>
             <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer.</h2>
             <p className='text-[#8892b0] py-4 max-w-[700px]'>I would consider myself a very thoughtful and creative individual. 
@@ -15,7 +15,7 @@ const Home = () => {
         of the needs of those around me. I am very curious, if I had 100 lifetimes 
         I would have no problem staying busy and learning new things in each and every one of them.</p>
             <div>
-                <button className='text-white group border-2 rounded px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Projects
+                <button className='text-white group border-2 rounded px-6 py-3 my-2 flex items-center hover:bg-[#41c69e] hover:border-[#41c69e]'>View Projects
                     <span className='group-hover:rotate-90 duration-300'>
                     <HiArrowNarrowRight className='ml-3'/>
                     </span>
