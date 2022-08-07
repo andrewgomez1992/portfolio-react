8116ec6ce8afb64293a395d6dbb9ef4c2086e449
Andrew_Gomez <keepitgomez@yahoo.com>
2022-08-06 19:49:23 -0700

    projects added

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -13,26 +13,125 @@ const Work = () => {
                 <p className='py-6'>Check out some of my recent projects</p>
             </div>
 
-            <div style={{backgroundImage: `url(${flix})`}}
-            className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-                <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center mx-auto content-div'>
+            {/** Container */}
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+                {/** Grid Item */}
+                <div style={{backgroundImage: `url(${flix})`}}
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center mx-auto content-div'>
                     
                     {/** Hover Effects */}
-                    <div>
-                        <span>
-
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            React JS & Firebase
                         </span>
-                        <div>
+                        <div className='pt-8 text-center'>
                             <a href="">
-                                <button></button>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
                             <a href="">
-                                <button></button>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
                     </div>
                 </div>
+                {/** Grid Item */}
+                <div style={{backgroundImage: `url(${myadmin})`}}
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center mx-auto content-div'>
+                    
+                    {/** Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            React JS & Sass
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                {/** Grid Item */}
+                <div style={{backgroundImage: `url(${devspace})`}}
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center mx-auto content-div'>
+                    
+                    {/** Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            React JS
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                {/** Grid Item */}
+                <div style={{backgroundImage: `url(${airtravel})`}}
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center mx-auto content-div'>
+                    
+                    {/** Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            HTML and CSS
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                {/** Grid Item */}
+                {/* <div style={{backgroundImage: `url(${flix})`}}
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center mx-auto content-div'>
+                     */}
+                    {/** Hover Effects */}
+                    {/* <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            React JS & Firebase
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div> */}
+                {/** Grid Item */}
+                {/* <div style={{backgroundImage: `url(${flix})`}}
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center mx-auto content-div'>
+                     */}
+                    {/** Hover Effects */}
+                    {/* <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            React JS & Firebase
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href="">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div> */}
+                {/* </div> */}
             </div>
+            
 
         </div>
     </div>
