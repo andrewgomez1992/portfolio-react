3f702dcaf4650cf3b667c6df4b0811f8bedc01cb
Andrew_Gomez <keepitgomez@yahoo.com>
2022-08-06 20:17:59 -0700

    lets join forces button complete

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,10 @@ const Contact = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-[#41c69e] text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>Submit the form below or shoot me an email - drewgomez209@gmail.com</p>
             </div>
-            <input type="text" placeholder='Name' name='name' />
+            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
+            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='name' />
+            <textarea className='bg-[#ccd6f6] p-2' placeholder='Message' name="message" rows="10"></textarea>
+            <button className='text-white border-2 hover:bg-[#41c69e] hover:border-[#41c69e] px-4 py-3 my-8 mx-auto flex items-center rounded'>Let's join forces</button>
         </form>
     </div>
   )
