fe86d3569e8c1cf916b186fb1bdf0757f23ac7dd
Andrew_Gomez <keepitgomez@yahoo.com>
2022-08-06 20:29:37 -0700

    getform connected

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-        <form action="" className='flex flex-col max-w[600px] w-full'>
+        <form method='POST' action="https://getform.io/f/407b4886-1beb-4682-84ad-41ddf78303a0" className='flex flex-col max-w[600px] w-full'>
             <div>
                 <p className='text-4xl font-bold inline border-b-4 border-[#41c69e] text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>Submit the form below or shoot me an email - drewgomez209@gmail.com</p>
             </div>
-            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='name' />
-            <textarea className='bg-[#ccd6f6] p-2' placeholder='Message' name="message" rows="10"></textarea>
+            <input className='bg-[#ccd6f6] p-2 rounded' type="text" placeholder='Name' name='name' />
+            <input className='my-4 p-2 bg-[#ccd6f6] rounded' type="email" placeholder='Email' name='name' />
+            <textarea className='bg-[#ccd6f6] p-2 rounded' placeholder='Message' name="message" rows="10"></textarea>
             <button className='text-white border-2 hover:bg-[#41c69e] hover:border-[#41c69e] px-4 py-3 my-8 mx-auto flex items-center rounded'>Let's join forces</button>
         </form>
     </div>
