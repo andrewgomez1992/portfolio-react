db26768a92ab7614dc9f594268beff5877606bc8
Andrew_Gomez <keepitgomez@yahoo.com>
2022-08-06 20:08:39 -0700

    name input added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,9 @@ const Contact = () => {
         <form action="" className='flex flex-col max-w[600px] w-full'>
             <div>
                 <p className='text-4xl font-bold inline border-b-4 border-[#41c69e] text-gray-300'>Contact</p>
+                <p className='text-gray-300 py-4'>Submit the form below or shoot me an email - drewgomez209@gmail.com</p>
             </div>
+            <input type="text" placeholder='Name' name='name' />
         </form>
     </div>
   )
