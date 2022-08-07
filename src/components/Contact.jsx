e5ec1522137b8acef62ed36b83897d536a1dfb3f
Andrew_Gomez <keepitgomez@yahoo.com>
2022-08-06 20:07:14 -0700

    contact header styled

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen bg-[#0a192f]'>
-
+    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+        <form action="" className='flex flex-col max-w[600px] w-full'>
+            <div>
+                <p className='text-4xl font-bold inline border-b-4 border-[#41c69e] text-gray-300'>Contact</p>
+            </div>
+        </form>
     </div>
   )
 }
